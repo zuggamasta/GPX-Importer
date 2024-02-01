f9d14b99163009a7d0fd6ac021c45f5e858d7be9
Alexander Mitzkus <alexander.mitzkus@denkwerk.com>
2024-02-01 10:00:01 +0100

    fixed function names and added comments as well es TODOs

--- gpximporter.py
+++ gpximporter.py
@@ -37,17 +37,19 @@ def points_to_mesh(vertices,create_edges):
     gpx_collection.objects.link(gpx_object)
 
 
-def read_some_data(context, filepath, use_some_setting):
+def read_xml_data(context, filepath, use_some_setting):
     
     print(filepath)    
     tree = ET.parse(filepath)
     root = tree.getroot()
     
+    # TODO: Remove the "is_original" check and always just iterate over the contents of <trkseg></trkseg> instead of the naiive root[1][1] children
+
     is_original = False
-    
+    # Original Apple Health GPX files have additional data
     if root[0].tag == "{http://www.topografix.com/GPX/1/1}metadata":
         is_original = True
-        print("Yes. This is a original Apple File")
+        print("This is a original Apple File")
     
     try:
         print("running read_gpx... printing *.gpx metadata")
@@ -60,6 +62,7 @@ def read_some_data(context, filepath, use_some_setting):
                 waypoint.append(0)
                 waypoints.append(waypoint)   
         else:
+            # This is just tested for one other GPX type
             for child in root[0][0]:
                 waypoint = list(map(float,child.attrib.values()))
                 waypoint.append(0)
@@ -70,7 +73,6 @@ def read_some_data(context, filepath, use_some_setting):
         points_to_mesh(vertices,use_some_setting)
     except:
         print("import failed")
-    
 
     return {'FINISHED'}
 
@@ -82,9 +84,9 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty, CollectionProp
 from bpy.types import Operator
 
 
-class ImportSomeData(Operator, ImportHelper):
+class ImportGPXData(Operator, ImportHelper):
     """This appears in the tooltip of the operator and in the generated docs"""
-    bl_idname = "import_test.some_data"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_idname = "import_test.gpx_data"  # important since its how bpy.ops.import_test.gpx_data is constructed
     bl_label = "Import Some Data"
 
     # ImportHelper mixin class uses this
@@ -99,19 +101,9 @@ class ImportSomeData(Operator, ImportHelper):
     # List of operator properties, the attributes will be assigned
     # to the class instance from the operator settings before calling.
     use_setting: BoolProperty(
-        name="Example Boolean",
-        description="Example Tooltip",
+        name="Create Edges",
+        description="Creates connecting edges from each datapoint to the next",
         default=True,
-    )
-
-    type: EnumProperty(
-        name="Example Enum",
-        description="Choose between two items",
-        items=(
-            ('OPT_A', "First Option", "Description one"),
-            ('OPT_B', "Second Option", "Description two"),
-        ),
-        default='OPT_A',
     )
 
     ###########################################
@@ -127,26 +119,25 @@ class ImportSomeData(Operator, ImportHelper):
     ###########################################
 
     def execute(self, context):
-        gpx_collection = bpy.data.collections.new('gpx_collection')
         for current_file in self.files:
             filepath = os.path.join(self.directory, current_file.name)
-            read_some_data(context, filepath, self.use_setting)
+            read_xml_data(context, filepath, self.use_setting)
         return {'FINISHED'}
 
 
 # Only needed if you want to add into a dynamic menu.
 def menu_func_import(self, context):
-    self.layout.operator(ImportSomeData.bl_idname, text="Text Import Operator")
+    self.layout.operator(ImportGPXData.bl_idname, text="Text Import Operator")
 
 
 # Register and add to the "file selector" menu (required to use F3 search "Text Import Operator" for quick access).
 def register():
-    bpy.utils.register_class(ImportSomeData)
+    bpy.utils.register_class(ImportGPXData)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
-    bpy.utils.unregister_class(ImportSomeData)
+    bpy.utils.unregister_class(ImportGPXData)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
 
 
@@ -154,4 +145,4 @@ if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.import_test.some_data('INVOKE_DEFAULT')
+    bpy.ops.import_test.gpx_data('INVOKE_DEFAULT')
